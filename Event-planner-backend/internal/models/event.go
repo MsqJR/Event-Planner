1c327536552e997c69519532c2bd3a4e24111708
Ahmed Ezzidin <aahmedezidin191@gmail.com>
2025-11-28 03:41:26 +0200

    [AE] Added new attributes to the event json file structure

--- Event-planner-backend/internal/models/event.go
+++ Event-planner-backend/internal/models/event.go
@@ -1,12 +1,31 @@
 package models
+
 import "time"
 
 type Event struct {
 	ID          int       `json:"id"`
 	Name        string    `json:"name"`
-	Time        time.Time `json:"time"`
-	Date        time.Time `json:"date"`
+	Date        string    `json:"date"`
+	Time        string    `json:"time"`
+	Location    string    `json:"location"`
+	Description string    `json:"description"`
+	OrganizerID int       `json:"organizer_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateEventRequest struct {
+	Name        string `json:"name"`
+	Date        string `json:"date"`
+	Time        string `json:"time"`
 	Location    string `json:"location"`
 	Description string `json:"description"`
 }
 
+type UpdateEventRequest struct {
+	Name        string `json:"name"`
+	Date        string `json:"date"`
+	Time        string `json:"time"`
+	Location    string `json:"location"`
+	Description string `json:"description"`
+}
