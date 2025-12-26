a6e5b1577e9680d2557fe3433a229d0f71843ec4
MsqJR <msq>
2025-12-26 13:51:00 +0200

    Fix: Use VITE_API_URL environment variable

--- Event-planner-frontend/src/services/api.js
+++ Event-planner-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
+const API_BASE_URL = import.meta.env.VITE_API_URL 
 console.log("this is api url" + API_BASE_URL)
 
 const api = axios.create({
