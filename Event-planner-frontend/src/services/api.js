c9066e071d7039abe29c0c62914d755711f59753
MsqJR <msq>
2025-12-26 13:18:36 +0200

    env fix

--- Event-planner-frontend/src/services/api.js
+++ Event-planner-frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1'
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
+console.log("this is api url" + API_BASE_URL)
 
 const api = axios.create({
   baseURL: API_BASE_URL,
